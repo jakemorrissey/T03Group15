2674a9858edbc136ce5561aaf230d4372a4446a3
CarlosVeinti <47372279+CarlosVeinti@users.noreply.github.com>
2019-02-25 23:37:39 -0700

    Add files via upload

---
+++ Project/RunnerGame/src/PlayerInterface/Player.java
@@ -0,0 +1,38 @@
+/*Trex - 40 pixels wide 
+ *      43 pixels tall 
+ *      
+ *      Its jump must be > 46 pixels (height of single cactus)
+ */
+
+import java.util.ArrayList;
+
+public class Player {
+	
+	private int plX=0;  // starting x-coordinate for image (where top left is 0,0) (first column of pixels)
+	private int plY=137;  // starting y-coordinate for image (top row of pixels)
+	private int plWidth=40;
+	private int plHeight=43;
+	private Obstacles burrier;
+	// y range of canvas - 200 pixels
+	// floor starts at 180 pixels
+	// 180 (floor) - height of trex (43) = 137 Y - starting coordinate
+		
+	public boolean trackIntersection() {
+		
+	boolean collided = false;	
+		
+	int leftmost = burrier.getx();
+	int rightmost = leftmost + burrier.getWidth(); // rightmost pixel
+	
+	while (rightmost>=0) {
+		rightmost--;
+		
+		
+	}
+	return collided;
+	}
+	
+// Suppose pressing the jump button lifts avatar by 46 pixels.	
+// Suppose the floor background horizon line is set around 180 (y-axis) out of 200.
+	
+}
