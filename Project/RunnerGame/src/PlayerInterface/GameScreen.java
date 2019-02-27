9680f577b0660ed338ce49ed23aa3f23285d4136
jakemorrissey <47370732+jakemorrissey@users.noreply.github.com>
2019-02-26 18:15:50 -0700

    Update GameScreen.java

--- Project/RunnerGame/src/PlayerInterface/GameScreen.java
+++ Project/RunnerGame/src/PlayerInterface/GameScreen.java
@@ -1,12 +1,16 @@
-package PlayerInterface;
+package playerInterface;
 
 import java.awt.Color;
 import javax.swing.JPanel;
+import java.awt.event.MouseEvent;
+import java.util.Scanner;
 
 public class GameScreen extends JPanel implements Runnable {
 
 	int i = 0;
 	Thread thread;
+	int z = 500;
+
 	
 	public GameScreen() {
 		thread = new Thread(this);
@@ -15,15 +19,38 @@ public class GameScreen extends JPanel implements Runnable {
 	public void startGame() {
 		thread.start();
 	}
+
 	
 	public void run() {
 		while(true) {
-			System.out.println(i ++);
+			System.out.println(i ++);		
+			if (z >=150){
+				z --;}
 			try {
-				Thread.sleep(500);
+				Thread.sleep(z);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				e.printStackTrace();	
 			}
-		}
+			
+			if (i % 5 == 0){
+				System.out.println("Obstacle incoming, Press w and enter to jump");
+					long startTime = System.currentTimeMillis();
+					Scanner s= new Scanner(System.in);
+					char x = s.next().charAt(0);		
+						if (x == 'w'){
+							long elapsedTime = System.currentTimeMillis() - startTime;
+							if (elapsedTime <= (z*3)){							
+								System.out.println("Successfully jumped");}
+							else {System.out.println("Game Over");
+								System.exit(0);
+							 						
+				
+				}
+			}
+
+			
+			}
+
+	    }
 	}
 }
